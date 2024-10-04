2e0ea15861536ffd7d8663c0b132791387c41ebb
Tiago Santos <otiagosantos.code@gmail.com>
2024-10-04 15:31:15 -0300

    lz update.

--- lua/otiagosantos/plugins/lsp-config.lua
+++ lua/otiagosantos/plugins/lsp-config.lua
@@ -17,7 +17,7 @@ return {
 		'williamboman/mason-lspconfig.nvim',
 		config = function()
 			require('mason-lspconfig').setup({
-				ensure_installed = { 'lua_ls', 'intelephense', 'ts_ls' }
+				ensure_installed = { 'lua_ls', 'intelephense', 'ts_ls', 'csharp_ls' }
 			})
 		end
 	},
@@ -25,9 +25,23 @@ return {
 		'neovim/nvim-lspconfig',
 		config = function()
 			local lspconfig = require('lspconfig')
+
 			lspconfig.lua_ls.setup({})
-			lspconfig.intelephense.setup({})
+
+			lspconfig.intelephense.setup({
+				root_dir = lspconfig.util.root_pattern("composer.json", ".git", "index.php"), -- Arquivos que indicam o diretório raiz
+				settings = {
+					intelephense = {
+						files = {
+							maxSize = 5000000, -- Ajuste o tamanho máximo de arquivos se necessário
+						},
+					},
+				},
+			})
+
 			lspconfig.ts_ls.setup({})
+
+			lspconfig.csharp_ls.setup({})
 
 			--vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			--vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
@@ -35,4 +49,3 @@ return {
 	},
 
 }
-
